48e837fa2e1bc99f1a29083afd7aec259dcb6ba4
Denisse <denissepaolarojas@hotmail.com>
2016-08-12 23:33:40 -0600

    Inicio del GUI

---
+++ src/main.c
@@ -0,0 +1,40 @@
+#include <gtk/gtk.h>
+
+GtkWidget *label_test;
+
+int main(int argc, char *argv[])
+{
+    GtkBuilder      *builder;
+    GtkWidget       *window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "glade/try.glade", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "try_window"));
+    gtk_builder_connect_signals(builder, NULL);
+
+
+    label_test = GTK_WIDGET(gtk_builder_get_object(builder, "label_test"));
+
+
+    g_object_unref(builder);
+
+    gtk_widget_show(window);
+    gtk_main();
+
+    return 0;
+}
+
+void on_btn_algoritmo1_clicked()
+{
+    gtk_label_set_text(GTK_LABEL(label_test), "Esto es una pruebaa!");
+}
+
+
+// called when window is closed
+void on_window_main_destroy()
+{
+    gtk_main_quit();
+}
